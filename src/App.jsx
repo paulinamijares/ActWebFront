10502494bcd0c52511735889848883a97d699f42
paulinamijares <paumijares04@gmail.com>
2025-03-30 21:43:56 -0600

    update front

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import './App.css';
- 
+
 function App() {
   return (
     <div className="App">
-      Hola
+      <h2>Login</h2>
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter username"
+            value="user123"  // Static value
+            readOnly
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            value="password123"  // Static value
+            readOnly
+          />
+        </div>
+        <div>
+          <button type="submit" className="login-button">Login</button>
+        </div>
+      </form>
     </div>
   );
 }
