cab6c72ebe3db72b5d583bdf8741bcd422e645fe
paulinamijares <paumijares04@gmail.com>
2025-03-30 21:19:37 -0600

    update front

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage.jsx';
-import Dashboard from './components/Dashboard.jsx';
-import StandardImageList from './components/Contacto.jsx';
 import './App.css';
 import Footer from './components/Footer.jsx';
  
@@ -13,8 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/contacto" element={<StandardImageList />} />
         </Routes>
       </Router>
     </div>
