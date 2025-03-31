ba5ee88955fe8fddc0398f6ca602c47387f0a1cf
paulinamijares <paumijares04@gmail.com>
2025-03-30 21:57:45 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer.jsx'
 import Control from './components/Control.jsx'
 import Header from './components/Header.jsx'
  
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
