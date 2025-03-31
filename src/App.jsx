3732b7c13f7943f974e2613668b70114c9371b24
paulinamijares <paumijares04@gmail.com>
2025-03-30 21:56:37 -0600

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer.jsx'
 import Control from './components/Control.jsx'
