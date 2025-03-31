80b028aa6a16421adc3a25f688bf3c7e1592c108
paulinamijares <paumijares04@gmail.com>
2025-03-30 21:41:13 -0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './components/LoginPage.jsx';
 import './App.css';
  
 function App() {
   return (
     <div className="App">
-      <Footer />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </Router>
+      Hola
     </div>
   );
 }
