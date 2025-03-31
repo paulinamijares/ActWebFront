975296736e237284dd1cc4de5c22864a87dafcb0
paulinamijares <paumijares04@gmail.com>
2025-03-30 20:42:20 -0600

    update front

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ResponsiveAppBar from './NavBar.jsx';
-import { Typography, Container, Grid, Box, colors } from '@mui/material';
+import { Typography, Container, Grid, Box } from '@mui/material';
 
 
 export default function ContactPage() {
