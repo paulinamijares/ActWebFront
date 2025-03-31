5adce8ed6ff25db47af0da8e85548927f428561a
paulinamijares <paumijares04@gmail.com>
2025-03-30 20:40:47 -0600

    update front

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage.jsx';
 import Dashboard from './components/Dashboard.jsx';
